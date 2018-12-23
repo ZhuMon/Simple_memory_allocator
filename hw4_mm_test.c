d65cb4430ea6c6b4b6ede811e8da3f8edc5583c0
ZhuMon <richardlin0212@gmail.com>
2018-12-23 14:47:34 +0800

    Add Application program(initial)

--- hw4_mm_test.c
+++ hw4_mm_test.c
@@ -3,6 +3,19 @@
 
 int main(int argc, char *argv[])
 {
+    while(1) {
+        char command[20] = {};
+        char tmp_char;
+        printf("\e[1;32mmyshell]e[0m$ ");
 
+        scanf("%s", command);
+        if(strcmp(command, "alloc") == 0) {
+
+        } else if( strcmp(command, "free") == 0) {
+
+        } else if( strcmp(command, "print") == 0) {
+
+        }
+    }
     return 0;
 }
